fc4acbc0878e46c2fc5fc8eec1886cd96b689f3d
Philipp Wolfer <ph.wolfer@gmail.com>
2021-07-20 09:06:39 +0200

    Avoid building HttpClient for MusicBrainzClient multiple times

--- app/src/main/java/org/musicbrainz/picard/barcodescanner/webservice/MusicBrainzClient.kt
+++ app/src/main/java/org/musicbrainz/picard/barcodescanner/webservice/MusicBrainzClient.kt
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2021 Philipp Wolfer <ph.wolfer@gmail.com>
+ * Copyright (C) 2021 Akshat Tiwari
+ *
+ * This file is part of MusicBrainz Picard Barcode Scanner.
+ *
+ * MusicBrainz Picard Barcode Scanner is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * MusicBrainz Picard Barcode Scanner is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MusicBrainz Picard Barcode Scanner. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.musicbrainz.picard.barcodescanner.webservice
 
 import com.google.gson.GsonBuilder
@@ -7,26 +27,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MusicBrainzClient {
 
-    private val baseUrl = "https://musicbrainz.org/ws/2/"
-    private var userAgent = "picard-android-barcodescanner/1.5"
-
     suspend fun queryReleasesByBarcode(barcode: String?): ReleaseSearchResponse {
         val query = "barcode:%s".format(barcode)
         return instance.queryReleases(query)
     }
-
-    private val okHttpClient = HttpClient.newBuilder()
-        .addInterceptor { chain ->
-            val original = chain.request()
-
-            val requestBuilder = original.newBuilder()
-                .header("User-Agent", userAgent)
-                .addHeader("Accept", "application/json")
-                .method(original.method(), original.body())
-
-            val request = requestBuilder.build()
-            chain.proceed(request)
-        }.build()
 
     private val instance: MusicBrainzApi by lazy {
         val retrofit = Retrofit.Builder()
